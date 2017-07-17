4a904ad8d093c9707b1fa9204381eaf3a75bcfee
Martian2218 <mathematicalmartians@gmail.com>
2017-07-17 14:16:08 -0700

    pokecode

--- src/com/company/Azumarill.java
+++ src/com/company/Azumarill.java
@@ -2,6 +2,8 @@ package com.company;
 
 import edu.digipen.gameobject.GameObject;
 
+import javax.xml.soap.Text;
+
 /**
  * Created by yuuki.sawanoi on 7/17/2017.
  */
@@ -9,6 +11,9 @@ public class Azumarill extends GameObject
 {
     public Azumarill()
     {
-        super("Azumarill",250,250,"Azumarill.png");
+        super("Azumarill",200,200,"Azumarill2.png");
+        setPosition(-300, -150);
+
     }
+
 }
