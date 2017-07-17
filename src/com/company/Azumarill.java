1e193a20ce0072a42c75ed7429e12b11ba030eec
BigPapi34ever <ken5shiro@hotmail.com>
2017-07-17 15:03:16 -0700

    Pics

--- src/com/company/Azumarill.java
+++ src/com/company/Azumarill.java
@@ -1,9 +1,6 @@
 package com.company;
 
-import edu.digipen.InputManager;
 import edu.digipen.gameobject.GameObject;
-
-import java.awt.event.KeyEvent;
 
 /**
  * Created by kengo.shirokane on 7/17/2017.
@@ -16,12 +13,6 @@ public class Azumarill extends GameObject
         setPosition(-300, -150);
 
     }
-    //what the deuce do i do fix plz
-    if(InputManager.isPressed(KeyEvent.VK_UP));
-    {
-        float y = getPositionY();
-        y += 3;
-        setPositionY(y);
-    }
+
 
 }
