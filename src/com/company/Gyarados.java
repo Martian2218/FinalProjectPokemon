a323ebf2cb3e4d700fb4476e5b9230651d7bd2d6
BigPapi34ever <ken5shiro@hotmail.com>
2017-07-17 14:19:56 -0700

    Pics

---
+++ src/com/company/Gyarados.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import edu.digipen.gameobject.GameObject;
+
+/**
+ * Created by kengo.shirokane on 7/17/2017.
+ */
+public class Gyarados {
+    public class Gyarados extends GameObject
+    {
+        public Gyarados()
+        {
+            super("Gyarados",250,250,"Gyarados.png");
+        }
+    }
+
+}
