c9b222a4447d31712e0846b278a066e3eff58c59
BigPapi34ever <ken5shiro@hotmail.com>
2017-07-17 14:10:36 -0700

    Pics

--- src/com/company/Azumarill.java
+++ src/com/company/Azumarill.java
@@ -3,7 +3,7 @@ package com.company;
 import edu.digipen.gameobject.GameObject;
 
 /**
- * Created by yuuki.sawanoi on 7/17/2017.
+ * Created by kengo.shirokane on 7/17/2017.
  */
 public class Azumarill extends GameObject
 {
