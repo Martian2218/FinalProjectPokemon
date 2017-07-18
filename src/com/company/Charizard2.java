d5be5d2b21ed16832fb2f72b29e4c93d32498158
Martian2218 <mathematicalmartians@gmail.com>
2017-07-18 12:23:49 -0700

    pokecode

---
+++ src/com/company/Charizard2.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import edu.digipen.gameobject.GameObject;
+
+/**
+ * Created by yuuki.sawanoi on 7/18/2017.
+ */
+public class Charizard2 extends GameObject
+{
+    public Charizard2()
+    {
+        super("Charizard2", 200, 200, "Charizard2.png");
+        setPosition(300, 150);
+    }
+}
---
+++ src/com/company/Tyrantrum2.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import edu.digipen.gameobject.GameObject;
+
+/**
+ * Created by yuuki.sawanoi on 7/18/2017.
+ */
+public class Tyrantrum2 extends GameObject
+{
+    public Tyrantrum2()
+    {
+        super("Tyrantrum2", 200, 200, "Tyrantrum2.png");
+        setPosition(300, -200);
+    }
+}
